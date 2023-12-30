dc0dd01dcf3f1192429d6490b9c29b2741016d79
Mel Avina <melissaavina.ma@gmail.com>
2023-12-29 20:12:36 -0800

    added api error handling

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -5,9 +5,14 @@
 				class="py-2 px-1 w-full bg-transparent border-b focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]" />
 			<ul class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]"
 				v-if="mapboxSearchResults">
-				<li v-for="searchResult in mapboxSearchResults" :key="searchResult.id" class="py-2 cursor-pointer">
-					{{ searchResult.place_name }}
-				</li>
+				<p v-if="searchError">Sorry, something went wrong, please try again.</p>
+				<p v-if="!serverError && mapboxSearchResults.length === 0">No results match your query, try a different
+					term.</p>
+				<template v-else>
+					<li v-for="searchResult in mapboxSearchResults" :key="searchResult.id" class="py-2 cursor-pointer">
+						{{ searchResult.place_name }}
+					</li>
+				</template>
 			</ul>
 		</div>
 	</main>
@@ -22,16 +27,20 @@ const mapboxAPIKey =
 const searchQuery = ref("");
 const queryTimeout = ref(null);
 const mapboxSearchResults = ref(null);
+const searchError = ref(null);
 
 const getSearchResults = () => {
 	clearTimeout(queryTimeout.value);
 	queryTimeout.value = setTimeout(async () => {
 		if (searchQuery.value !== "") {
-			const result = await axios.get(
-				`https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapboxAPIKey}&types=place`
-			);
-			mapboxSearchResults.value = result.data.features;
-			console.log(mapboxSearchResults.value);
+			try {
+				const result = await axios.get(
+					`https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapboxAPIKey}&types=place`
+				);
+				mapboxSearchResults.value = result.data.features;
+			} catch {
+				searchError.value = true;
+			}
 			return;
 		}
 		mapboxSearchResults.value = null;
